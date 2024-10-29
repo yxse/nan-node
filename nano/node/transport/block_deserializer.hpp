b509b4604560fdd0d37fb226ef76d54ceaaa4dd0
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2024-10-29 14:14:28 +0100

    Move `block_deserializer` to `transport` namespace

--- nano/node/transport/block_deserializer.hpp
+++ nano/node/transport/block_deserializer.hpp
@@ -8,41 +8,36 @@
 #include <memory>
 #include <vector>
 
-namespace nano
+namespace nano::transport
 {
-class block;
+/**
+ * Class to read a block-type byte followed by a serialised block from a stream.
+ * It is typically used to read a series of block-types and blocks terminated by a not-a-block type.
+ */
+class block_deserializer : public std::enable_shared_from_this<block_deserializer>
+{
+public:
+	using callback_type = std::function<void (boost::system::error_code, std::shared_ptr<nano::block>)>;
 
-namespace bootstrap
-{
+	block_deserializer ();
 	/**
-	 * Class to read a block-type byte followed by a serialised block from a stream.
-	 * It is typically used to read a series of block-types and blocks terminated by a not-a-block type.
+	 * Read a type-prefixed block from 'socket' and pass the result, or an error, to 'callback'
+	 * A normal end to series of blocks is a marked by return no error and a nullptr for block.
 	 */
-	class block_deserializer : public std::enable_shared_from_this<nano::bootstrap::block_deserializer>
-	{
-	public:
-		using callback_type = std::function<void (boost::system::error_code, std::shared_ptr<nano::block>)>;
+	void read (nano::transport::tcp_socket & socket, callback_type const && callback);
 
-		block_deserializer ();
-		/**
-		 * Read a type-prefixed block from 'socket' and pass the result, or an error, to 'callback'
-		 * A normal end to series of blocks is a marked by return no error and a nullptr for block.
-		 */
-		void read (nano::transport::tcp_socket & socket, callback_type const && callback);
+private:
+	/**
+	 * Called by read method on receipt of a block type byte.
+	 * The type byte will be in the read_buffer.
+	 */
+	void received_type (nano::transport::tcp_socket & socket, callback_type const && callback);
 
-	private:
-		/**
-		 * Called by read method on receipt of a block type byte.
-		 * The type byte will be in the read_buffer.
-		 */
-		void received_type (nano::transport::tcp_socket & socket, callback_type const && callback);
+	/**
+	 * Called by received_type when a block is received, it parses the block and calls the callback.
+	 */
+	void received_block (nano::block_type type, callback_type const && callback);
 
-		/**
-		 * Called by received_type when a block is received, it parses the block and calls the callback.
-		 */
-		void received_block (nano::block_type type, callback_type const && callback);
-
-		std::shared_ptr<std::vector<uint8_t>> read_buffer;
-	};
-}
+	std::shared_ptr<std::vector<uint8_t>> read_buffer;
+};
 }
