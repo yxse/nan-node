5a66e7ea1ef2ec6d76d56896ece9eee5d7944c5c
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2024-11-18 19:41:20 +0100

    Fix priority threshold

--- nano/node/bootstrap/account_sets.cpp
+++ nano/node/bootstrap/account_sets.cpp
@@ -61,15 +61,18 @@ void nano::bootstrap::account_sets::priority_down (nano::account const & account
 	{
 		stats.inc (nano::stat::type::bootstrap_account_sets, nano::stat::detail::deprioritize);
 
-		if (it->fails >= account_sets::max_fails || it->fails >= it->priority)
+		auto priority = it->priority / account_sets::priority_divide;
+
+		if (it->fails >= account_sets::max_fails || it->fails >= it->priority || priority <= account_sets::priority_cutoff)
 		{
 			stats.inc (nano::stat::type::bootstrap_account_sets, nano::stat::detail::erase_by_threshold);
 			priorities.get<tag_account> ().erase (it);
 		}
 		else
 		{
-			priorities.get<tag_account> ().modify (it, [] (auto & val) {
+			priorities.get<tag_account> ().modify (it, [priority] (auto & val) {
 				val.fails += 1;
+				val.priority = priority;
 			});
 		}
 	}
