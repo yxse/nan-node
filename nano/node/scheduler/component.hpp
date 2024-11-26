queader/nano-node

Contains hash functionality

nano/node/scheduler/component.hpp
@@ -13,10 +13,11 @@ public:
 	explicit component (nano::node & node);
 	~component ();
 
-	// Starts all schedulers
 	void start ();
-	// Stops all schedulers
 	void stop ();
+
+	/// Does the block exist in any of the schedulers
+	bool exists (nano::block_hash const & hash) const;
 
 	nano::container_info container_info () const;
 

nano/node/scheduler/manual.cpp
@@ -46,6 +46,14 @@ void nano::scheduler::manual::push (std::shared_ptr<nano::block> const & block_a
 	notify ();
 }
 
+bool nano::scheduler::manual::exists (nano::block_hash const & hash) const
+{
+	nano::lock_guard<nano::mutex> lock{ mutex };
+	return std::any_of (queue.cbegin (), queue.cend (), [&hash] (auto const & item) {
+		return std::get<0> (item)->hash () == hash;
+	});
+}
+
 bool nano::scheduler::manual::predicate () const
 {
 	return !queue.empty ();

nano/node/scheduler/manual.hpp
@@ -37,6 +37,8 @@ public:
 	// Call action with confirmed block, may be different than what we started with
 	void push (std::shared_ptr<nano::block> const &, boost::optional<nano::uint128_t> const & = boost::none);
 
+	bool exists (nano::block_hash const &) const;
+
 	nano::container_info container_info () const;
 };
 }

nano/node/scheduler/priority.hpp
@@ -39,6 +39,7 @@ public:
 	bool activate (secure::transaction const &, nano::account const &);
 	bool activate (secure::transaction const &, nano::account const &, nano::account_info const &, nano::confirmation_height_info const &);
 
+	bool exists (nano::block_hash const &) const;
 	void notify ();
 	std::size_t size () const;
 	bool empty () const;

nano/node/vote_cache.cpp
@@ -200,6 +200,14 @@ std::vector<std::shared_ptr<nano::vote>> nano::vote_cache::find (const nano::blo
 	return {};
 }
 
+bool nano::vote_cache::exists (const nano::block_hash & hash) const
+{
+	nano::lock_guard<nano::mutex> lock{ mutex };
+
+	auto & cache_by_hash = cache.get<tag_hash> ();
+	return cache_by_hash.find (hash) != cache_by_hash.end ();
+}
+
 bool nano::vote_cache::erase (const nano::block_hash & hash)
 {
 	nano::lock_guard<nano::mutex> lock{ mutex };

nano/node/vote_router.hpp
@@ -39,6 +39,9 @@ public:
 	vote_router (nano::vote_cache & cache, nano::recently_confirmed_cache & recently_confirmed);
 	~vote_router ();
 
+	void start ();
+	void stop ();
+
 	// Add a route for 'hash' to 'election'
 	// Existing routes will be replaced
 	// Election must hold the block for the hash being passed in
@@ -54,9 +57,7 @@ public:
 	std::unordered_map<nano::block_hash, nano::vote_code> vote (std::shared_ptr<nano::vote> const &, nano::vote_source = nano::vote_source::live, nano::block_hash filter = { 0 });
 	bool active (nano::block_hash const & hash) const;
 	std::shared_ptr<nano::election> election (nano::block_hash const & hash) const;
-
-	void start ();
-	void stop ();
+	bool exists (nano::block_hash const & hash) const;
 
 	using vote_processed_event_t = nano::observer_set<std::shared_ptr<nano::vote> const &, nano::vote_source, std::unordered_map<nano::block_hash, nano::vote_code> const &>;
 	vote_processed_event_t vote_processed;

yxse/nan-node

nano/node/scheduler/component.hpp
@@ -16,6 +16,9 @@ public:
 	void start ();
 	void stop ();
 
+	/// Does the block exist in any of the schedulers
+	bool contains (nano::block_hash const & hash) const;
+
 	nano::container_info container_info () const;
 
 private:

nano/node/scheduler/manual.cpp
@@ -46,6 +46,14 @@ void nano::scheduler::manual::push (std::shared_ptr<nano::block> const & block_a
 	notify ();
 }
 
+bool nano::scheduler::manual::contains (nano::block_hash const & hash) const
+{
+	nano::lock_guard<nano::mutex> lock{ mutex };
+	return std::any_of (queue.cbegin (), queue.cend (), [&hash] (auto const & item) {
+		return std::get<0> (item)->hash () == hash;
+	});
+}
+
 bool nano::scheduler::manual::predicate () const
 {
 	return !queue.empty ();

nano/node/scheduler/manual.hpp
@@ -37,6 +37,8 @@ public:
 	// Call action with confirmed block, may be different than what we started with
 	void push (std::shared_ptr<nano::block> const &, boost::optional<nano::uint128_t> const & = boost::none);
 
+	bool contains (nano::block_hash const &) const;
+
 	nano::container_info container_info () const;
 };
 }

nano/node/scheduler/priority.hpp
@@ -40,6 +40,7 @@ public:
 	bool activate (nano::secure::transaction const &, nano::account const &, nano::account_info const &, nano::confirmation_height_info const &);
 	bool activate_successors (nano::secure::transaction const &, nano::block const &);
 
+	bool contains (nano::block_hash const &) const;
 	void notify ();
 	std::size_t size () const;
 	bool empty () const;

nano/node/vote_cache.cpp
@@ -202,6 +202,14 @@ std::vector<std::shared_ptr<nano::vote>> nano::vote_cache::find (const nano::blo
 	return {};
 }
 
+bool nano::vote_cache::contains (const nano::block_hash & hash) const
+{
+	nano::lock_guard<nano::mutex> lock{ mutex };
+
+	auto & cache_by_hash = cache.get<tag_hash> ();
+	return cache_by_hash.find (hash) != cache_by_hash.end ();
+}
+
 bool nano::vote_cache::erase (const nano::block_hash & hash)
 {
 	nano::lock_guard<nano::mutex> lock{ mutex };

nano/node/vote_router.hpp
@@ -40,6 +40,9 @@ public:
 	vote_router (nano::vote_cache & cache, nano::recently_confirmed_cache & recently_confirmed);
 	~vote_router ();
 
+	void start ();
+	void stop ();
+
 	// Add a route for 'hash' to 'election'
 	// Existing routes will be replaced
 	// Election must hold the block for the hash being passed in
@@ -55,9 +58,7 @@ public:
 	std::unordered_map<nano::block_hash, nano::vote_code> vote (std::shared_ptr<nano::vote> const &, nano::vote_source = nano::vote_source::live, nano::block_hash filter = { 0 });
 	bool active (nano::block_hash const & hash) const;
 	std::shared_ptr<nano::election> election (nano::block_hash const & hash) const;
-
-	void start ();
-	void stop ();
+	bool contains (nano::block_hash const & hash) const;
 
 	using vote_processed_event_t = nano::observer_set<std::shared_ptr<nano::vote> const &, nano::vote_source, std::unordered_map<nano::block_hash, nano::vote_code> const &>;
 	vote_processed_event_t vote_processed;