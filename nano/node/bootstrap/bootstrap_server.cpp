queader/nano-node

Node traffic prioritization

nano/node/bootstrap/bootstrap_server.cpp
@@ -104,7 +104,7 @@ bool nano::bootstrap_server::verify (const nano::asc_pull_req & message) const
 	return std::visit (verify_visitor{}, message.payload);
 }
 
-bool nano::bootstrap_server::request (nano::asc_pull_req const & message, std::shared_ptr<nano::transport::channel> channel)
+bool nano::bootstrap_server::request (nano::asc_pull_req const & message, std::shared_ptr<nano::transport::channel> const & channel)
 {
 	if (!verify (message))
 	{
@@ -113,8 +113,7 @@ bool nano::bootstrap_server::request (nano::asc_pull_req const & message, std::s
 	}
 
 	// If channel is full our response will be dropped anyway, so filter that early
-	// TODO: Add per channel limits (this ideally should be done on the channel message processing side)
-	if (channel->max (nano::transport::traffic_type::bootstrap))
+	if (channel->max (nano::transport::traffic_type::bootstrap_server))
 	{
 		stats.inc (nano::stat::type::bootstrap_server, nano::stat::detail::channel_full, nano::stat::dir::in);
 		return false;
@@ -171,13 +170,9 @@ void nano::bootstrap_server::respond (nano::asc_pull_ack & response, std::shared
 	on_response.notify (response, channel);
 
 	channel->send (
-	response, [this] (auto & ec, auto size) {
-		if (ec)
-		{
-			stats.inc (nano::stat::type::bootstrap_server, nano::stat::detail::write_error, nano::stat::dir::out);
-		}
-	},
-	nano::transport::buffer_drop_policy::limiter, nano::transport::traffic_type::bootstrap);
+	response, nano::transport::traffic_type::bootstrap_server, [this] (auto & ec, auto size) {
+		stats.inc (nano::stat::type::bootstrap_server, to_stat_detail (ec), nano::stat::dir::out);
+	});
 }
 
 void nano::bootstrap_server::run ()
@@ -220,7 +215,7 @@ void nano::bootstrap_server::run_batch (nano::unique_lock<nano::mutex> & lock)
 
 		transaction.refresh_if_needed ();
 
-		if (!channel->max (nano::transport::traffic_type::bootstrap))
+		if (!channel->max (nano::transport::traffic_type::bootstrap_server))
 		{
 			auto response = process (transaction, request);
 			respond (response, channel);

nano/node/repcrawler.cpp
@@ -246,7 +246,7 @@ std::vector<std::shared_ptr<nano::transport::channel>> nano::rep_crawler::prepar
 	// Crawl more aggressively if we lack sufficient total peer weight.
 	auto const required_peer_count = sufficient_weight ? conservative_count : aggressive_count;
 
-	auto random_peers = node.network.random_set (required_peer_count, 0, /* include channels with ephemeral remote ports */ true);
+	auto random_peers = node.network.random_set (required_peer_count);
 
 	auto should_query = [&, this] (std::shared_ptr<nano::transport::channel> const & channel) {
 		if (auto rep = reps.get<tag_channel> ().find (channel); rep != reps.get<tag_channel> ().end ())
@@ -325,8 +325,6 @@ void nano::rep_crawler::query (std::vector<std::shared_ptr<nano::transport::chan
 
 	for (const auto & channel : target_channels)
 	{
-		debug_assert (channel != nullptr);
-
 		bool tracked = track_rep_request (hash_root, channel);
 		if (tracked)
 		{
@@ -336,15 +334,9 @@ void nano::rep_crawler::query (std::vector<std::shared_ptr<nano::transport::chan
 			auto const & [hash, root] = hash_root;
 			nano::confirm_req req{ network_constants, hash, root };
 
-			channel->send (
-			req,
-			[this] (auto & ec, auto size) {
-				if (ec)
-				{
-					stats.inc (nano::stat::type::rep_crawler, nano::stat::detail::write_error, nano::stat::dir::out);
-				}
-			},
-			nano::transport::buffer_drop_policy::no_socket_drop);
+			channel->send (req, nano::transport::traffic_type::rep_crawler, [this] (auto & ec, auto size) {
+				stats.inc (nano::stat::type::rep_crawler_send, to_stat_detail (ec), nano::stat::dir::out);
+			});
 		}
 		else
 		{

yxse/nan-node

nano/node/bootstrap/bootstrap_server.cpp
@@ -104,7 +104,7 @@ bool nano::bootstrap_server::verify (const nano::asc_pull_req & message) const
 	return std::visit (verify_visitor{}, message.payload);
 }
 
-bool nano::bootstrap_server::request (nano::asc_pull_req const & message, std::shared_ptr<nano::transport::channel> channel)
+bool nano::bootstrap_server::request (nano::asc_pull_req const & message, std::shared_ptr<nano::transport::channel> const & channel)
 {
 	if (!verify (message))
 	{
@@ -113,8 +113,7 @@ bool nano::bootstrap_server::request (nano::asc_pull_req const & message, std::s
 	}
 
 	// If channel is full our response will be dropped anyway, so filter that early
-	// TODO: Add per channel limits (this ideally should be done on the channel message processing side)
-	if (channel->max (nano::transport::traffic_type::bootstrap))
+	if (channel->max (nano::transport::traffic_type::bootstrap_server))
 	{
 		stats.inc (nano::stat::type::bootstrap_server, nano::stat::detail::channel_full, nano::stat::dir::in);
 		return false;
@@ -171,13 +170,9 @@ void nano::bootstrap_server::respond (nano::asc_pull_ack & response, std::shared
 	on_response.notify (response, channel);
 
 	channel->send (
-	response, [this] (auto & ec, auto size) {
-		if (ec)
-		{
-			stats.inc (nano::stat::type::bootstrap_server, nano::stat::detail::write_error, nano::stat::dir::out);
-		}
-	},
-	nano::transport::buffer_drop_policy::limiter, nano::transport::traffic_type::bootstrap);
+	response, nano::transport::traffic_type::bootstrap_server, [this] (auto & ec, auto size) {
+		stats.inc (nano::stat::type::bootstrap_server_ec, to_stat_detail (ec), nano::stat::dir::out);
+	});
 }
 
 void nano::bootstrap_server::run ()
@@ -220,7 +215,7 @@ void nano::bootstrap_server::run_batch (nano::unique_lock<nano::mutex> & lock)
 
 		transaction.refresh_if_needed ();
 
-		if (!channel->max (nano::transport::traffic_type::bootstrap))
+		if (!channel->max (nano::transport::traffic_type::bootstrap_server))
 		{
 			auto response = process (transaction, request);
 			respond (response, channel);

nano/node/repcrawler.cpp
@@ -260,7 +260,7 @@ std::deque<std::shared_ptr<nano::transport::channel>> nano::rep_crawler::prepare
 	// Crawl more aggressively if we lack sufficient total peer weight.
 	auto const required_peer_count = sufficient_weight ? conservative_count : aggressive_count;
 
-	auto random_peers = node.network.random_set (required_peer_count, 0, /* include channels with ephemeral remote ports */ true);
+	auto random_peers = node.network.random_set (required_peer_count);
 
 	auto should_query = [&, this] (std::shared_ptr<nano::transport::channel> const & channel) {
 		if (auto rep = reps.get<tag_channel> ().find (channel); rep != reps.get<tag_channel> ().end ())
@@ -339,8 +339,6 @@ void nano::rep_crawler::query (std::deque<std::shared_ptr<nano::transport::chann
 
 	for (const auto & channel : target_channels)
 	{
-		debug_assert (channel != nullptr);
-
 		bool tracked = track_rep_request (hash_root, channel);
 		if (tracked)
 		{
@@ -350,15 +348,9 @@ void nano::rep_crawler::query (std::deque<std::shared_ptr<nano::transport::chann
 			auto const & [hash, root] = hash_root;
 			nano::confirm_req req{ network_constants, hash, root };
 
-			channel->send (
-			req,
-			[this] (auto & ec, auto size) {
-				if (ec)
-				{
-					stats.inc (nano::stat::type::rep_crawler, nano::stat::detail::write_error, nano::stat::dir::out);
-				}
-			},
-			nano::transport::buffer_drop_policy::no_socket_drop);
+			channel->send (req, nano::transport::traffic_type::rep_crawler, [this] (auto & ec, auto size) {
+				stats.inc (nano::stat::type::rep_crawler_ec, to_stat_detail (ec), nano::stat::dir::out);
+			});
 		}
 		else
 		{